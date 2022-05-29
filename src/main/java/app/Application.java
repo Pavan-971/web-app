3acf32c1af2400689726979ea0ca2bb66f0bad19
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2022-05-29 14:47:10 -0500

    Update Application.java

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,7 @@
 public class Application {
 
     public static void main(String[] args) {
-      System.out.println("jokfff ffgerggggergerhhf");
+      System.out.println("jokfff hhhh");
     }
 }
 
